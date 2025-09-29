e9d631300906fcaa7ae690bd1c24588d01f592c1
Bharat <bbhushan175@gmail.com>
2025-09-29 15:12:17 -0400

    Update quiz.py with new logic/fixes

--- quiz.py
+++ quiz.py
@@ -1,4 +1,5 @@
-score = 0
+score_Y = 0
+score_N = 0
 ask = input("do you want to play the game: ").lower()
 if ask != "yes":
     print("the game is closed")
@@ -9,33 +10,37 @@ answer1 = input("Is your computer with RAM more than 12GB: ").lower()
 
 if answer1 == "yes":
     print("great")
-    score += 1
+    score_Y += 1
 else:
-    print("incorrect")
+    print("no")
+    score_N += 1
 
 answer2 = input("Is you computer having big screen: ").lower()
 
 if answer2 == "yes":
     print("great")
-    score +=1
+    score_Y += 1
 else:
     print("not possible to play on this screen")
+    score_N += 1
 
 answer3 = input("Is you computer having speakers: ").lower()
 
 if answer3 == "yes":
     print("great")
-    score +=1
+    score_Y += 1
 else:
     print("not possible to play with this speaker")
+    score_N += 1
 
 
 answer4 = input("are you having joystick: ").lower()
 
 if answer4 == "yes":
     print("great")
-    score +=1
+    score_Y += 1
 else:
-    print("not possible to play on this screen")
+    print("not possible to play without joystick")
+    score_N += 1
 
-print(f"As per the anser with YES the score {score}")
+print(f"As per the answer with YES the score :{score_Y} and with NO the score is {score_N}")
